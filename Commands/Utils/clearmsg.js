ecb5ce9e2896074fe8987fa384fc1fb1157683a0
Unknown-user-dev <jumperkylian@outlook.com>
2024-11-19 02:31:20 +0100

    ✨ | Added clearmsg.js (Command)

---
+++ Commands/Utils/clearmsg.js
@@ -0,0 +1,24 @@
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
+
+const clearCommand = {
+    name: 'clear',
+    description: lang.cleardesc,
+    run(client, message, args) {
+         message.channel.messages.fetch({ limit: 100 }).then(messages => {
+          messages.forEach(message => {
+                if (message.author.id === client.user.id) {
+                 message.delete()
+                }
+          })
+        })
+
+
+    },
+};
+
+// Register Commands
+client.commands = new Map();
+client.commands.set(clearCommand.name, clearCommand);
+
+module.exports = clearCommand;
