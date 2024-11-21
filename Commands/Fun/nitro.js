b8976b4afd4b3b2643208db25c7652e45d432124
Clyde <166905445+clydedc@users.noreply.github.com>
2024-11-21 21:07:39 +0400

    Add files via upload

---
+++ Commands/Fun/nitro.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js-selfbot-v13");
+
+module.exports = {
+  name: "nitro",
+  description: "Generate a fake Nitro gift link",
+  run: async (client, message, args) => {
+    try {
+      function nitrocode(length, charset) {
+        const charsets = {
+          "0": "0123456789",
+          "a": "abcdefghijklmnopqrstuvwxyz",
+          "A": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        };
+
+        let chars = "";
+        if (charset.includes("0")) chars += charsets["0"];
+        if (charset.includes("a")) chars += charsets["a"];
+        if (charset.includes("A")) chars += charsets["A"];
+
+        let result = "";
+        for (let i = 0; i < length; i++) {
+          result += chars[Math.floor(Math.random() * chars.length)];
+        }
+
+        return result;
+      }
+
+      const fakeNitroLink = `https://discord.gift/${nitrocode(16, "0aA")}`;
+      
+      await message.edit(fakeNitroLink);
+    } catch (error) {
+      console.error("Error while running the 'nitro' command:", error);
+    }
+  },
+};
---
+++ Commands/Fun/thot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js-selfbot-v13");
+
+module.exports = {
+  name: "thot",
+  description: "Check the percentage of someone's filthiness",
+  run: async (client, message, args) => {
+    try {
+      const target = message.mentions.users.first() || message.author;
+      const rating = Math.floor(Math.random() * 100) + 1;
+
+      const response = `Le pourcentage de la saloperie de ${target.username} est de: \`${rating}%\``;
+      await message.edit(response);
+    } catch (error) {
+      console.error("An error occurred while running the 'thot' command:", error);
+    }
+  },
+};
